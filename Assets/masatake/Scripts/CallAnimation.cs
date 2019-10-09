7145cc5cfd556bcda27a4de10c8d9af60efb4209
DESKTOP-SBQ2E7T\masatake <mstkwkgw@gmail.com>
2019-10-09 12:33:21 +0900

    // 剣を振るアニメーションを作成しました。(ver1.5)

--- Assets/masatake/Scripts/CallAnimation.cs
+++ Assets/masatake/Scripts/CallAnimation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test4 : MonoBehaviour {
+public class CallAnimation : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
