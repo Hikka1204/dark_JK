a84fc68c0d7ce2857b5348df85efe9d1aff64bf1
DESKTOP-SIINF0D\takay <hideto-t-17e@stu.kbc.ac.jp>
2019-10-10 10:35:40 +0900

    [変更]フェードを少し変更しました

--- Assets/hideto/Result/Result_SC/TextFadeIn.cs
+++ Assets/hideto/Result/Result_SC/TextFadeIn.cs
@@ -17,16 +17,23 @@ public class TextFadeIn : MonoBehaviour {
     // Uiテキストを格納
     public GameObject NowScore;
 
+    // スコアフェードイン完了フラグ
+    public static bool scorefadeinflg;
+
 	// Use this for initialization
 	void Start () {
         // スコアのカラーを取得してアルファ値を0に初期化
         textcolor = this.NowScore.GetComponent<Text>().color;
         textcolor.a = 0;
         this.NowScore.GetComponent<Text>().color = textcolor;
+
+        // フェードインフラグをfalse
+        scorefadeinflg = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        // 画面をクリックするとスコアをすぐに表示
         if(Input.GetMouseButtonDown(0))
         {
             scoretextintime = 0.0f;
@@ -37,10 +44,21 @@ public class TextFadeIn : MonoBehaviour {
 
     void FadeIn()
     {
+        // スコアのテキストのアルファ値をだんだん上げる
         if(textcolor.a <= 1)
         {
             textcolor.a += textfadespeed;
             this.NowScore.GetComponent<Text>().color = textcolor;
         }
+        else if(textcolor.a >= 1)
+        {
+            scorefadeinflg = true;
+            getScoreFadeInFlg();
+        }
+    }
+
+    public static bool getScoreFadeInFlg()
+    {
+        return scorefadeinflg;
     }
 }
